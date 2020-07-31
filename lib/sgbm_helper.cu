c1cbdd9b0f15f46fa8dbac38d172e53591a74b24
Alex Armbruster <alex.armbruster@elderresearch.com>
2020-07-31 15:56:12 -0400

    diagonal top left -> bottom right written not tested

--- lib/sgbm_helper.cu
+++ lib/sgbm_helper.cu
@@ -149,13 +149,14 @@ __global__ void r_aggregate(float *dp, float *cost_image, int m, int n)
     }
   
     MinArray[threadIdx.y][threadIdx.x] = prev_min;
-    //MinArray[threadIdx.y][threadIdx.x] = (float) row * n + col;
     __syncthreads();
-  
   
     float d0 = 0;
     float d1 = 0;
     float d2 = 0;
+    
+    // when processing a video stream, need to make sure that processing of multiple
+    // frames can overlap, since after this point only one warp of threads is executing
 
     // threads from only one warp will handle rightward aggregation across the
     // region that has been loaded into shared memory
@@ -238,6 +239,64 @@ __global__ void vertical_aggregate(float *dp, float *cost_image,
     col += blockDim.x;
     }
   }
+
+
+
+
+
+
+
+  __global__ void diagonal_tl_br_aggregate(float *dp, float *cost_image, 
+    int m, int n)
+    {
+      // which column of array to work on
+      int start_col = blockDim.x * blockIdx.x + threadIdx.x + 1;
+      int depth_dim_size = m*n;
+      
+      // todo: maybe it will work better to take running average of every d 
+      // slices
+      while(start_col < n)
+      {
+        int col = start_col;
+        for (int row = 1; row < m; row++)
+        {
+          //int arr_ind = 0;
+          float prev_min = 100000000.0;
+          int ind = (row - 1) * n + col - 1;
+          
+          // calculate min cost disparity for this column from row-1
+          //#pragma unroll
+          for (int depth = 0; depth < D; depth+=D_STEP){
+            prev_min = fminf(dp[ind], prev_min);
+            ind += (depth_dim_size * D_STEP);
+          }
+  
+          float d0 = 0;
+          float d1 = 0;
+          float d2 = 0;
+          float d3 = prev_min + (float) P2;
+          ind = (row - 1) * n + col - 1;
+          int current_ind = row * n + col;
+  
+          
+          // todo: try having this loop go from 1 to d-1 and removing the if else
+          for (int d = 0; d < D; d+=D_STEP){
+            // for each d I need dp[{d-1, d, d+1}, row-1, col],
+            dp[current_ind] = cost_image[current_ind] + dp_criteria(dp, ind, depth_dim_size, d, (float) P1, (float) P2, &d0, &d1, &d2, &d3);
+            ind += (depth_dim_size * D_STEP);
+            current_ind += (depth_dim_size * D_STEP);
+          }
+
+          col += 1
+          if col == n // wrap each thread around once it gets to the last column
+            col = 1;
+
+        }
+      start_col += blockDim.x;
+      }
+    }
+
+
 
 
 
