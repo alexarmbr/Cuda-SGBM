42c92edb9ec5c950fffd3900f4ca44fd107523e5
arm-buster <ubuntu@ip-172-31-46-199.ec2.internal>
2020-08-08 15:48:07 +0000

    cuda aggregation fully working, generated correct image

--- python_stereo/run_sgbm_cuda.py
+++ python_stereo/run_sgbm_cuda.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     stereo = SemiGlobalMatching(im1, im2, os.path.join("../data", IMAGE_DIR ,"calib.txt"),
     window_size=3, resize=(640,480))
 
-    params = {"p1":5, "p2":90000, "census_kernel_size":7, "reversed":True}
+    params = {"p1":5, "p2":90000, "census_kernel_size":7, "reversed":False}
     stereo.set_params(params)
     stereo.params['ndisp'] = 50
     t1 = time()
@@ -43,13 +43,10 @@ if __name__ == "__main__":
     else:
         D = reversed(range(int(-stereo.params['ndisp']), 1))
     cost_images = stereo.compute_disparity_img(cim1, cim2, D)
-    cost_images = np.float32(cost_images)
     m, n, D = cost_images.shape
-    
-
-    #t1 = time()
     L = stereo.aggregate_cost(cost_images)
-    cost_images = np.float32(cost_images)
+    #cost_images = np.float32(cost_images)
+    
     #print("python aggregate cost %f" % (time() - t1))
     L = L.transpose((2,0,1))
     
@@ -97,8 +94,8 @@ if __name__ == "__main__":
     vertical_aggregate_down(dp_ptr, cost_images_ptr, rows, cols, block = (256, 1, 1), grid = (1,1))
     vertical_aggregate_up(dp_ptr, cost_images_ptr, rows, cols, block = (256, 1, 1), grid = (1,1))
     
-    #r_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (shmem_size, shmem_size, 1), grid = (1, vertical_blocks))
-    #l_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (shmem_size, shmem_size, 1), grid = (1, vertical_blocks))
+    r_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (shmem_size, shmem_size, 1), grid = (1, vertical_blocks))
+    l_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (shmem_size, shmem_size, 1), grid = (1, vertical_blocks))
     
     diagonal_tl_br_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (256, 1, 1), grid = (1,1))
     diagonal_bl_tr_aggregate(dp_ptr, cost_images_ptr, rows, cols, block = (256, 1, 1), grid = (1,1))
@@ -116,7 +113,10 @@ if __name__ == "__main__":
     gt = np.argmin(L, axis=0)
     agg_image = agg_image.transpose((2,1,0))
     min_cost_im += 1
+    gt += 1
     im = stereo.compute_depth(np.int32(min_cost_im))
     gt_im = stereo.compute_depth(np.int32(gt))
+    im = stereo.normalize(im, 0.1)
+    gt_im = stereo.normalize(gt_im, 0.1)
     np.save("../out_images/testim.npy", im)
     np.save("../out_images/gtim.npy", im)
